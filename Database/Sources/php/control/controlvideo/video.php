7f7f623e641dc802805a8ff617c3e30e26be9f9d
Your Name <you@example.com>
2018-12-25 10:46:41 +0700

    delete file Database/Sources

--- Database/Sources/php/control/controlvideo/video.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $href=$_GET['link'];
-    echo"<script>alert($href)</script>";
-    echo'
-      <video width="100%" height="auto" src="'.$href.'" controls autoplay ></video>  ';   
-   header("location:../../templates.php")
-?>
